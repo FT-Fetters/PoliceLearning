fa9f95010a904f317462480293c2d584464b6cdf
FT-Fetters <1438255191@qq.com>
2021-11-18 09:37:02 +0800

    Initial commit

---
+++ src/main/java/com/lyun/policelearning/PoliceLearningApplication.java
@@ -0,0 +1,13 @@
+package com.lyun.policelearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliceLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoliceLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lyun/policelearning/PoliceLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyun.policelearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliceLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
