de7d982cabc9a06259d9ad7ae78e3262e99bd2dc
DMH695 <2313978908@qq.com>
2021-12-12 18:49:00 +0800

    修改

--- src/main/java/com/lyun/policelearning/controller/information/InformationManageApi.java
+++ src/main/java/com/lyun/policelearning/controller/information/InformationManageApi.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.ClassUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
