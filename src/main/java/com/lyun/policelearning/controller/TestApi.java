93994343892d3299728bc74fd59f0623557a6436
FT-Fetters <1438255191@qq.com>
2022-01-27 20:42:59 +0800

    fix: 修复buggggggggggg

--- src/main/java/com/lyun/policelearning/controller/TestApi.java
+++ src/main/java/com/lyun/policelearning/controller/TestApi.java
@@ -4,11 +4,19 @@ import com.lyun.policelearning.utils.PathTools;
 import com.lyun.policelearning.utils.ResultBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/test")
+@RestController
 public class TestApi {
     @RequestMapping(value = "/run",method = RequestMethod.GET)
     public Object run(){
-        return new ResultBody<>(true,200, PathTools.getRunPath());
+        try {
+            throw new Exception("exp");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getStackTrace();
+        }
+        //return new ResultBody<>(true,200, PathTools.getRunPath());
     }
 }
--- src/main/java/com/lyun/policelearning/utils/FileUtils.java
+++ src/main/java/com/lyun/policelearning/utils/FileUtils.java
@@ -3,20 +3,19 @@ package com.lyun.policelearning.utils;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class FileUtils {
 
-    public static String txt2String(File file){
+    public static String txt2String(File file) throws IOException {
         StringBuilder result = new StringBuilder();
-        try{
+        if (file.setReadable(true)) {
             BufferedReader br = new BufferedReader(new FileReader(file));//构造一个BufferedReader类来读取文件
             String s = null;
             while((s = br.readLine())!=null){//使用readLine方法，一次读一行
                 result.append(System.lineSeparator()).append(s);
             }
             br.close();
-        }catch(Exception e){
-            e.printStackTrace();
         }
         return result.toString();
     }
