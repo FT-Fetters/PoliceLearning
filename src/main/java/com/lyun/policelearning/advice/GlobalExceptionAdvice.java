100388c81e91a45504eec163b67bc64d601ff9ca
FT-Fetters <1438255191@qq.com>
2022-01-28 17:28:17 +0800

    Revert "fix: 返回错误消息"
    This reverts commit 22e5c087461c069613273f1547f2a01177a130ba.

--- src/main/java/com/lyun/policelearning/advice/GlobalExceptionAdvice.java
+++ src/main/java/com/lyun/policelearning/advice/GlobalExceptionAdvice.java
@@ -4,24 +4,21 @@ import com.lyun.policelearning.utils.LogUtils;
 import com.lyun.policelearning.utils.ResultBody;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.security.SignatureException;
 import java.util.Arrays;
 
 @ControllerAdvice
 public class GlobalExceptionAdvice {
 
-    @ResponseBody
     @ExceptionHandler(value = Exception.class)
-    public Object handleHttpException(HttpServletRequest req, Exception ex){
+    public String handleHttpException(HttpServletRequest req, Exception ex){
         ex.printStackTrace();
         LogUtils.log(ex.getMessage(),"error",false,null);
-        return new ResultBody<>(false,-1,ex.getMessage());
+        return "/error";
     }
 
 
