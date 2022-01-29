3047dcd59d85bdc587c210bc832f3fbbc1d14ebf
DMH695 <2313978908@qq.com>
2022-01-29 10:31:26 +0800

    fix:添加

--- src/main/java/com/lyun/policelearning/advice/GlobalExceptionAdvice.java
+++ src/main/java/com/lyun/policelearning/advice/GlobalExceptionAdvice.java
@@ -21,8 +21,10 @@ public class GlobalExceptionAdvice {
     public Object handleHttpException(HttpServletRequest req, Exception ex){
         ex.printStackTrace();
         LogUtils.log(ex.getMessage(),"error",false,null);
+        System.out.println(ex.getMessage());
+        if (ex.getMessage().equals("失效，请重新登录。")){
+            return new ResultBody<>(false,-2,ex.getMessage());
+        }
         return new ResultBody<>(false,-1,ex.getMessage());
     }
-
-
 }
