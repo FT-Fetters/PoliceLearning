c208f123b0d2498ee5a4e5b79fc4ca4aa5c3cd00
白兰鸽 <2313978908@qq.com>
2022-10-04 17:35:38 +0800

    feat: 富文本、Excel模板和导入

--- src/main/java/com/lyun/policelearning/entity/Law.java
+++ src/main/java/com/lyun/policelearning/entity/Law.java
@@ -1,14 +1,29 @@
 package com.lyun.policelearning.entity;
 
+import com.alibaba.excel.annotation.ExcelIgnore;
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.write.style.ColumnWidth;
 import lombok.Data;
 
 @Data
 public class Law {
+    @ExcelIgnore
     private int id;
+    @ExcelProperty("法律类型")
     private String lawtype;
+    @ExcelProperty("法律名称")
+    @ColumnWidth(20)
     private String title;
+    @ExcelProperty("法律内容")
+    @ColumnWidth(50)
     private String content;
+    @ExcelProperty("释义阐明")
+    @ColumnWidth(50)
     private String explaination;
+    @ExcelProperty("罪名解析")
+    @ColumnWidth(50)
     private String crime;
+    @ExcelProperty("关键词")
+    @ColumnWidth(50)
     private String keyword;
 }
--- src/main/java/com/lyun/policelearning/entity/LawType.java
+++ src/main/java/com/lyun/policelearning/entity/LawType.java
@@ -1,11 +1,18 @@
 package com.lyun.policelearning.entity;
 
+import com.alibaba.excel.annotation.ExcelIgnore;
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.write.style.ColumnWidth;
 import lombok.Data;
 
 //建立法律类型与具体法律的关系
 @Data
 public class LawType {
+    @ExcelIgnore
     private int id;
+    @ExcelProperty("法律类型")
+    @ColumnWidth(20)
     private String lawtype;
+    @ExcelIgnore
     private String title;
 }
