8699043e72bc45ce52df55db215a1642690fef7f
FT-Fetters <1438255191@qq.com>
2023-03-28 17:25:42 +0800

    feat: 日志审计

--- src/main/java/com/lyun/policelearning/utils/log/AuditSysLogSender.java
+++ src/main/java/com/lyun/policelearning/utils/log/AuditSysLogSender.java
@@ -5,7 +5,9 @@ import com.alibaba.fastjson.JSONObject;
 import com.lyun.policelearning.entity.SysLog;
 import com.lyun.policelearning.utils.factory.AuditSysLogFactory;
 import com.lyun.policelearning.utils.factory.support.LogFactory;
+import com.lyun.policelearning.utils.log.config.RestTemplateConfig;
 import com.lyun.policelearning.utils.log.support.LogSender;
+import lombok.SneakyThrows;
 import org.junit.Assert;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -16,10 +18,12 @@ import org.springframework.web.client.RestTemplate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AuditSysLogSender implements LogSender {
 
-    private static final String URL_PREFIX = "https://44.10.1.134/api";
+    private static final String URL_PREFIX = "http://localhost:8081/api";
+//    private static final String URL_PREFIX = "https://44.10.1.134/api";
     private static final String URL_HEARTBEAT = "/kafka/heartbeat";
     private static final String URL_ACCEPT_LOGS = "/kafka/acceptLogs";
     private static final String[] HEADER_KEYS = new String[]{
@@ -42,7 +46,7 @@ public class AuditSysLogSender implements LogSender {
         return instance;
     }
 
-    private static final Integer QUEUE_MAX_SIZE = 5;
+    private static final Integer QUEUE_MAX_SIZE = 0;
 
     private final List<SysLog> logQueue = new ArrayList<>();
 
@@ -50,25 +54,29 @@ public class AuditSysLogSender implements LogSender {
 
     @Override
     public void send(SysLog sysLog) {
-        if (logQueue.size() > QUEUE_MAX_SIZE){
-            this.send();
-        }else {
-            logQueue.add(sysLog);
+        logQueue.add(sysLog);
+        if (logQueue.size() < QUEUE_MAX_SIZE){
+            return;
         }
+        this.send();
     }
 
+    @SneakyThrows
     @Override
     public void heart() {
         HttpHeaders headers = new HttpHeaders();
         for (int i = 0; i < HEADER_KEYS.length; i++) {
             headers.add(HEADER_KEYS[i],HEADER_VALUES[i]);
         }
-        RestTemplate restTemplate = new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate(RestTemplateConfig.generateHttpRequestFactory());
+        Map<String,String> map = new HashMap<>();
+        map.put("test","test");
         //System.out.println("**************************" + restTemplate);
-        JSONObject result = restTemplate.postForObject(URL_PREFIX + URL_HEARTBEAT, new HttpEntity<String>(headers), JSONObject.class);
+        JSONObject result = restTemplate.postForObject(URL_PREFIX + URL_HEARTBEAT, new HttpEntity<>(map,headers), JSONObject.class);
         System.out.println("已执行完心跳连接" + result);
     }
 
+    @SneakyThrows
     private void send(){
         JSONObject bodyJson = new JSONObject();
         bodyJson.put("Logs", logQueue);
@@ -77,27 +85,27 @@ public class AuditSysLogSender implements LogSender {
         for (int i = 0; i < HEADER_KEYS.length; i++) {
             headers.add(HEADER_KEYS[i],HEADER_VALUES[i]);
         }
-        RestTemplate restTemplate = new RestTemplate();
-        JSONObject result = restTemplate.postForObject(URL_PREFIX + URL_ACCEPT_LOGS, new HttpEntity<>(new HashMap<>(), headers), JSONObject.class);
-        //JSONObject result = restTemplate.postForObject(url, new HttpEntity<>(new HashMap<>(),headers), JSONObject.class);
-        assert result != null;
-        Assert.assertFalse( result.getBoolean("success"));
-        Assert.assertEquals(HttpStatus.NOT_ACCEPTABLE.value(), result.get("code"));
-
-        result = restTemplate.postForObject(URL_PREFIX + URL_ACCEPT_LOGS, new HttpEntity<>(body, headers), JSONObject.class);
-        System.out.println("已执行完推送" + result);
-        //result = restTemplate.postForObject(url, new HttpEntity<>(body,headers), JSONObject.class);
-        assert result != null;
-        Assert.assertFalse( result.getBoolean("success"));
-        Assert.assertEquals(HttpStatus.NOT_ACCEPTABLE.value(), result.get("code"));
-
+        RestTemplate restTemplate = new RestTemplate(RestTemplateConfig.generateHttpRequestFactory());
+//        JSONObject result = restTemplate.postForObject(URL_PREFIX + URL_ACCEPT_LOGS, new HttpEntity<>(new HashMap<>(), headers), JSONObject.class);
+//        //JSONObject result = restTemplate.postForObject(url, new HttpEntity<>(new HashMap<>(),headers), JSONObject.class);
+//        assert result != null;
+//        Assert.assertFalse( result.getBoolean("success"));
+//        Assert.assertEquals(HttpStatus.NOT_ACCEPTABLE.value(), result.get("code"));
+//
+//        result = restTemplate.postForObject(URL_PREFIX + URL_ACCEPT_LOGS, new HttpEntity<>(body, headers), JSONObject.class);
+//        System.out.println("已执行完推送" + result);
+//        //result = restTemplate.postForObject(url, new HttpEntity<>(body,headers), JSONObject.class);
+//        assert result != null;
+//        Assert.assertFalse( result.getBoolean("success"));
+//        Assert.assertEquals(HttpStatus.NOT_ACCEPTABLE.value(), result.get("code"));
+//
         headers.add("batchNumber", "5");
-        result = restTemplate.postForObject(URL_PREFIX + URL_ACCEPT_LOGS, new HttpEntity<>(body, headers), JSONObject.class);
-        assert result != null;
-        Assert.assertFalse(result.getBoolean("success"));
-        Assert.assertEquals(HttpStatus.PAYMENT_REQUIRED.value(), result.get("code"));
+//        result = restTemplate.postForObject(URL_PREFIX + URL_ACCEPT_LOGS, new HttpEntity<>(body, headers), JSONObject.class);
+//        assert result != null;
+//        Assert.assertFalse(result.getBoolean("success"));
+//        Assert.assertEquals(HttpStatus.PAYMENT_REQUIRED.value(), result.get("code"));
 
-        result = restTemplate.postForObject(URL_PREFIX + URL_ACCEPT_LOGS, new HttpEntity<>(body, headers), JSONObject.class);
+        JSONObject result = restTemplate.postForObject(URL_PREFIX + URL_ACCEPT_LOGS, new HttpEntity<>(body, headers), JSONObject.class);
         assert result != null;
         Assert.assertTrue(result.getBoolean("success"));
         Assert.assertEquals(HttpStatus.OK.value(), result.get("code"));
